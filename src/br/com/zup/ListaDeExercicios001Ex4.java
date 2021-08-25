f5c7aa79b0243cf020338e69e79c16ddad4de4f3
Michelle Kênia <keniamsoaress@gmail.com>
2021-08-25 10:44:08 -0300

    Exercicio4

--- src/br/com/zup/ListaDeExercicios001Ex4.java
+++ src/br/com/zup/ListaDeExercicios001Ex4.java
@@ -1,2 +1,31 @@
-package br.com.zup;public class ListaDeExercicios001Ex4 {
+package br.com.zup;
+
+public class ListaDeExercicios001Ex4 {
+    public static void main(String[] args) {
+
+        //Armazenando o valor por hora trabalhada
+        int valorPorHoraTrabalhada = 10;
+
+        //Dando print valor da hora
+        System.out.println("Valor da hora trabalhada: " + valorPorHoraTrabalhada + "R$");
+
+        //Armazenando a quantidade de horas trabalhadas por dia
+        int horasTrabalhadasPorDia = 6;
+
+        //Armazenando a quantidade de horas trabalhadas por mês
+        int diasUteisDoMes = 22;
+        int horasTrabalhadasPorMes = diasUteisDoMes * horasTrabalhadasPorDia;
+
+        //Dando print horas trabalhadas no mês
+        System.out.println("Horas trabalhadas no mês: " + horasTrabalhadasPorMes + "Hrs");
+
+        //Armazenando o salário do mês
+        int salarioDoMes = horasTrabalhadasPorMes * valorPorHoraTrabalhada;
+
+        //Dando print
+        System.out.println("Salário do mês : " +salarioDoMes + "R$");
+
+
+
+    }
 }
