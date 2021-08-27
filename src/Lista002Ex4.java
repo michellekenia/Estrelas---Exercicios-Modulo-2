ae95116c28e2470d6ab7e6ae47dbb1a34e8d5dee
Michelle Kênia <keniamsoaress@gmail.com>
2021-08-27 11:50:04 -0300

    Lista 002 Exercicio 4_Alteracao.

--- src/Lista002Ex4.java
+++ src/Lista002Ex4.java
@@ -13,25 +13,32 @@ public class Lista002Ex4 {
 
         // Declarando as variáveis de porcentagens, aumentos e novos salários
 
+        double porcentagem20 = 0.2;
         double porcentagem15 = 0.15;
         double porcentagem10 = 0.10;
         double porcentagem5 = 0.05;
 
+        double aumento20 = salarioatual * porcentagem20;
         double aumento15 = salarioatual * porcentagem15;
         double aumento10 = salarioatual * porcentagem10;
         double aumento5 = salarioatual * porcentagem5;
 
+        double novosalario20 = (salarioatual + aumento20);
         double novosalario15 = (salarioatual + aumento15);
         double novosalario10 = (salarioatual + aumento10);
         double novosalario5 = (salarioatual + aumento5);
 
         // Declarando as condições
 
-        if (salarioatual> 280 & salarioatual <700){
+        if (salarioatual > 0 & salarioatual <= 280) {
+            System.out.println("Seu aumento será de 20%. " + "No valor de: " + aumento20 + "."
+                    + " Portanto, seu salário com reajuste será: " + novosalario20 + ".");
+        }
+        else if (salarioatual> 280 & salarioatual <= 700){
             System.out.println("Seu aumento será de 15%. " + "No valor de: " + aumento15 + "."
                     + " Portanto, seu salário com reajuste será: " + novosalario15 + ".");
         }
-        else if (salarioatual> 700 & salarioatual<1500) {
+        else if (salarioatual> 700 & salarioatual <= 1500) {
             System.out.println("Seu aumento será de 10%. " + "No valor de: " + aumento10 + "."
                     + " Portanto, seu salário com reajuste será: " + novosalario10 + ".");
         }
@@ -40,7 +47,7 @@ public class Lista002Ex4 {
                     + " Portanto, seu salário com reajuste será: " + novosalario5);
         }
         else {
-            System.out.println("Por favor, digite um salário válido.");
+            System.out.println("Por favor, digite um valor válido.");
         }
     }
 }
