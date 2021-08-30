c9ce7b7e8cb0079286ead92cc0cdb920f9add847
Michelle Kênia <keniamsoaress@gmail.com>
2021-08-30 14:51:18 -0300

    Lista 04 Ex.1

--- src/br/com/zup/Lista004Ex1.java
+++ src/br/com/zup/Lista004Ex1.java
@@ -1,4 +1,21 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
+//Faça um programa que mostra na tela os números de 1 a 100.
+
 public class Lista004Ex1 {
+    public static void main(String[] args) {
+
+        System.out.println("Números de 1 a 100 :");
+
+        int contador = 1;
+        while (contador <= 100) {
+            System.out.println(contador);
+            contador ++;
+        }
+
+    }
+
+
 }
