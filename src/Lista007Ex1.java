96c4225f802374770771c9b6cb5888a1c6dbf60b
Michelle Kênia <keniamsoaress@gmail.com>
2021-09-01 19:42:05 -0300

    Lista 7 Ex1

--- src/Lista007Ex1.java
+++ src/Lista007Ex1.java
@@ -1,2 +1,25 @@
+
+/* Escreva um programa que escreva na tela, de 1 até 100, de 1 em 1, 2 vezes. A
+primeira vez com "for" e a segunda com "while". */
 public class Lista007Ex1 {
+    public static void main(String[] args) {
+
+        System.out.println("Usando While");
+        System.out.println("Números de 1 a 100:" );
+
+        int contador = 1;
+        while (contador <= 100) {
+
+            System.out.println(contador);
+
+            contador ++;
+        }
+
+        System.out.println("Usando For");
+        System.out.println("Números de 1 a 100:" );
+        for (int contador1 = 1; contador1 <=100; contador1++) {
+            System.out.println(contador1);
+        }
+
+    }
 }
