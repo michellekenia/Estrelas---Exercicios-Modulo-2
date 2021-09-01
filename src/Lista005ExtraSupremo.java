49472877cb3788cc7cb4f1ca3b516a57843360de
Michelle Kênia <keniamsoaress@gmail.com>
2021-08-31 23:13:22 -0300

    Exercicio Extra Lista 5

---
+++ src/Lista005ExtraSupremo.java
@@ -0,0 +1,59 @@
+/*
+Faça um programa que receba um conjunto de N números (Digitado pelo usuário) e
+mostre:
+a. O menor valor
+b. O maior valor
+c. A soma dos valores.
+ */
+
+import java.util.Scanner;
+
+public class Lista005ExtraSupremo {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        //Escrevendo as variáveis
+        int contador = 1;
+        double qtdDeNumeros;
+        double valorDigitado;
+        double menorValor = 0;
+        double maiorValor = 0;
+        double somaDosValores = 0;
+
+        //Perguntando ao usuário a quantidade de números
+        System.out.println(" Digite a quantidade de números : ");
+        qtdDeNumeros = leitor.nextDouble();
+
+        //Iniciando o contador
+        while (contador <= qtdDeNumeros) {
+            System.out.println(" Digite o número " + contador + ":");
+            valorDigitado = leitor.nextDouble();
+
+            //Dando as condições
+            if (contador == 1) {
+                menorValor = valorDigitado;
+                maiorValor = valorDigitado;
+
+                else (valorDigitado <= menorValor); {
+                    if ()
+
+
+                }
+
+
+            }
+
+            somaDosValores = somaDosValores + valorDigitado;
+
+            contador++;
+        }
+
+        System.out.println(" O menor valor digitado é : " + menorValor);
+        System.out.println("O maior valor digitado é : " + maiorValor);
+        System.out.println("A soma dos números digitados é : " + somaDosValores);
+
+
+    }
+
+}
+
