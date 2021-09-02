27ac1ac62a60d01c18821c9caca8860ebd5b4229
Michelle Kênia <keniamsoaress@gmail.com>
2021-09-02 15:21:26 -0300

    Novo Commit Listas de Exercicios

--- src/Lista006Ex2.java
+++ src/Lista006Ex2.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Lista006Ex2 {
+/* Faça um Programa que leia um número e exiba o dia correspondente da
+semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
+ */
+
+import java.util.Scanner;
+
+public class Lista006Ex2 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        //Variáveis
+        int diaDesejado;
+        int domingo = 1;
+        int segunda = 2;
+        int terça = 3;
+        int quarta = 4;
+        int quinta = 5;
+        int sexta = 6;
+        int sabado = 7;
+
+
+        // Solicitando ao usuário o dia
+        System.out.println("Digite o número desejado: ");
+        diaDesejado = leitor.nextInt();
+
+        //Condições
+
+        if (diaDesejado == domingo) {
+            System.out.println("DOMINGO");
+
+        } else if (diaDesejado == segunda) {
+            System.out.println("SEGUNDA FEIRA");
+
+        } else if (diaDesejado == terça) {
+            System.out.println("TERÇA FEIRA");
+
+        } else if (diaDesejado == quarta) {
+            System.out.println("QUARTA FEIRA");
+
+        } else if (diaDesejado == quinta) {
+            System.out.println("QUINTA FEIRA");
+
+        } else if (diaDesejado == sexta) {
+            System.out.println("SEXTA FEIRA");
+
+        } else if (diaDesejado == sabado) {
+            System.out.println("SÁBADO");
+
+        } else {
+            System.out.println("Digite um número válido entre 1 e 7.");
+        }
+    }
 }
--- src/Lista006Ex3.java
+++ src/Lista006Ex3.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Lista006Ex3 {
+/*Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer
+número inteiro entre 1 a 10. O usuário deve informar de qual número ele deseja ver
+a tabuada */
+
+import java.util.Scanner;
+
+public class Lista006Ex3 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int contador = 1;
+        int numeroMaximo = 10;
+        int numeroDigitado = 0;
+        int multiplicador = 0;
+
+        System.out.println("Digite o número que deseja saber a tabuada: ");
+
+        while (contador <= numeroMaximo) {
+            numeroDigitado = leitor.nextInt();
+
+            System.out.println("Tabuada do " + numeroDigitado + ":");
+
+            while (contador <= numeroMaximo) {
+                multiplicador = numeroDigitado * contador;
+
+                System.out.println(numeroDigitado + " x " + contador + " = " + multiplicador);
+
+                contador++;
+            }
+
+            contador++;
+
+        }
+
+
+    }
 }
