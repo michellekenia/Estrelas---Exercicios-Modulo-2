d83f54b5966b889333d2b69b1ebea0e7ae289458
Michelle Kênia <keniamsoaress@gmail.com>
2021-09-03 20:34:33 -0300

    Exercicio 8 Resolvido.

--- src/Exercicio8.java
+++ src/Exercicio8.java
@@ -1,7 +1,4 @@
-/*Faça um Programa que leia uma lista de 5 números inteiros e mostre-os.
- */
-
-/* TENTATIVA DE CÓDIGO, NÃO DEU CERTO. TENTAR DESCOBRIR COMO FAZER.
+/*Faça um Programa que leia uma lista de 5 números inteiros e mostre-os.*/
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,40 +8,14 @@ public class Exercicio8 {
     public static void main(String[] args) {
 
         Scanner leitor = new Scanner(System.in);
-        List <Integer> numeros = new ArrayList <>();
+        List<Integer> numeros = new ArrayList<>();
 
-        //Variáveis
-        Integer numero1 = 0;
-        Integer numero2 = 0;
-        Integer numero3 = 0;
-        Integer numero4 = 0;
-        Integer numero5 = 0;
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Insira o número " + i + ": ");
+            numeros.add(leitor.nextInt());
+        }
 
-        System.out.println("Insira o primeiro número: ");
-        numero1 = leitor.nextInt();
-
-        System.out.println("Insira o segundo número: ");
-        numero1 = leitor.nextInt();
-
-        System.out.println("Insira o terceiro número: ");
-        numero1 = leitor.nextInt();
-
-        System.out.println("Insira o quarto número: ");
-        numero1 = leitor.nextInt();
-
-        System.out.println("Insira o quinto número: ");
-        numero1 = leitor.nextInt();
-
-
-        System.out.println(numeros);
-
-
-
-
-
+        System.out.println("Esta é a lista: " + numeros);
 
     }
 }
-
-
- */
