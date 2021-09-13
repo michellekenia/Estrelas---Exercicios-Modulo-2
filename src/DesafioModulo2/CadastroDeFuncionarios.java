c37a43bbbe89d3529b2b1f8107dc815fc1a9ff35
Michelle Kênia <keniamsoaress@gmail.com>
2021-09-13 16:39:32 -0300

    Refatoração de código

--- src/DesafioModulo2/CadastroDeFuncionarios.java
+++ src/DesafioModulo2/CadastroDeFuncionarios.java
@@ -1,42 +1,48 @@
+/*
+Crie um programa para gerenciar uma lista de funcionários de uma empresa,
+cada funcionário tem nome, telefone, email e CPF. No final do programa deve ser
+exibida a lista de funcionários cadastrados.
+Entrega Mínima: O sistema permite, via terminal, inserir pelo menos 1 funcionário
+em uma lista e retornar o mesmo na tela.
+Entrega Média: O sistema permite adicionar mais de um funcionário e apresenta
+um menu para decidir se deverá:
+1. Adicionar mais funcionários.
+2. Exibir a lista de todos os funcionários cadastrados.
+3. Encerrar o programa.
+Entrega Máxima: O Sistema permite excluir um funcionário usando como
+parâmetro o CPF e não permite inserir um funcionário com o CPF repetido.
+ */
+
 package DesafioModulo2;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Desafio {
+public class CadastroDeFuncionarios {
     public static void main(String[] args) {
 
-        // Iniciando Scanner e a estrutura HashMap.
         Scanner leitor = new Scanner(System.in);
         Map<String, String> listaDeFuncionarios = new HashMap<String, String>();
 
-        // Usando o tipo boolean para criar o menu.
-        // A variáveil iniciar armazena a resposta do leitor no menu.
-        // A variváel repetido será usada para descobrir se existe e-mail repetido no cadastro.
-        boolean iniciar = true;
         int resposta = 0;
         int repetido = 0;
 
-        System.out.println("Bem vinde, você está no Sistema De Inclusão De Colaboradores.");
+        System.out.println("Bem vinde, você está no Sistema De Inclusão De Funcionários.");
 
-        // Criar o menu para o usuário.
-        while (iniciar = true) {
-            System.out.println("Digite 1 para incluir um colaborador.");
-            System.out.println("Digite 2 para mostrar a lista de colaboradores cadastrados.");
+        while (true) {
+            System.out.println("Digite 1 para incluir um funcionário.");
+            System.out.println("Digite 2 para mostrar a lista de funcionários cadastrados.");
             System.out.println("Digite 3 para sair do sistema.");
-            System.out.println("Digite 4 excluir um cadastro.");
+            System.out.println("Digite 4 para excluir um cadastro.");
 
-            // Ler a resposta do usuário referente ao menu.
             resposta = leitor.nextInt();
             leitor.nextLine();
 
-            // Incluir os dados de cadastramento.
             if (resposta == 1) {
                 System.out.println("Digite o CPF: ");
                 String chave = leitor.nextLine();
 
-                // Verificar se o CPF digitado já está no cadastro.
                 for (String verificação : listaDeFuncionarios.keySet()) {
                     if (chave.equals(verificação)) {
                         repetido = 1;
@@ -59,31 +65,25 @@ public class Desafio {
 
                     String valor = nome + "," + telefone + "," + eMail;
 
-                    // Adicionar a chave e o valor na lista.
                     listaDeFuncionarios.put(chave, valor);
                 }
 
 
-                // Mostrar a lista de funcionários.
             } else if (resposta == 2) {
 
-                System.out.println("Essa é a lista de colaboradores cadastrados: " + listaDeFuncionarios);
+                System.out.println("Essa é a lista de funcionários cadastrados: " + listaDeFuncionarios);
 
-                // Finalizar o programa.
             } else if (resposta == 3) {
                 return;
 
-                // Excluir um funcionário da lista a partir da chave CPF.
             } else if (resposta == 4) {
-                System.out.println("Digite o CPF do colaborador que deseja excluir do cadastro: ");
+                System.out.println("Digite o CPF do funcionário que deseja excluir do cadastro: ");
                 String exclusao = leitor.nextLine();
 
                 listaDeFuncionarios.remove(exclusao);
 
-                // Mensagem para números inválidos no menu.
-            } else if (resposta != 1 && resposta != 2 && resposta != 3 && resposta != 4) {
+            } else {
                 System.out.println("Por favor, escolha um dos números disponíveis no menu.");
-
             }
 
         }
